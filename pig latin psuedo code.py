246ba751910d8309ac6a0447a075587bc9159605
kfmarini <55286533+kfmarini@users.noreply.github.com>
2019-09-27 18:07:21 -0400

    Add files via upload

---
+++ pig latin psuedo code.py
@@ -0,0 +1,6 @@
+#Kathryn Marini 9/27/19
+ask the user for a word to translate into pig latin
+create a list of vowels
+if the word begins with a vowel, then add 'yay' to the end of it for the new word
+if the word does not begin with a vowel then write the word without the first letter and add 'ay' to the end of it for the new word
+display the new word for the user
---
+++ pig latin.py
@@ -0,0 +1,9 @@
+#Kathryn Marini 2/27/19
+def pigLatin():
+    word = input('What would you like to say in pig latin?')
+    vowelList = ['a','e','i','o','u']
+    if( word[0:1] in vowelList):
+        newWord = word[0:] + 'yay'
+    else:
+        newWord = word[1:] + word[0] +'ay'
+    print(newWord)    
